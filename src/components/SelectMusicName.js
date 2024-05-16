6708d97f29562396d6b07cc56ba426f04c8d0326
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2024-05-16 23:20:01 +0200

    add thumbnail to music and video

--- src/components/SelectMusicName.js
+++ src/components/SelectMusicName.js
@@ -1,12 +1,11 @@
 import React from 'react'; 
 import { Panel } from 'primereact/panel';
+import NameCompleteMusic from './NameCompleteMusic';
 
 export default function SelectMusicName() {
     return (
         <Panel header="Scarica attraverso il nome" toggleable collapsed={true}>
-            <p className="m-0">
-            <input type="text" id="linkMusic" placeholder="Inserisci il nome della canzone"/>
-            </p>
+            <NameCompleteMusic />
         </Panel>
     )
 }
--- src/components/SelectVideoName.js
+++ src/components/SelectVideoName.js
@@ -1,12 +1,11 @@
 import React from 'react'; 
 import { Panel } from 'primereact/panel';
+import NameCompleteVideo from './NameCompleteVideo';
 
 export default function SelectVideoName() {
     return (
         <Panel header="Scarica attraverso il nome" toggleable collapsed={true}>
-            <p className="m-0">
-            <input type="text" id="linkMusic" placeholder="Inserisci il nome del video"/>
-            </p>
+            <NameCompleteVideo />
         </Panel>
     )
 }
