af4d59daea769fb49779d449ecce47c9638f94a5
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2024-05-21 21:53:02 +0200

    fix nameMusicBox

--- src/components/basicsComponent/SearchLabel.js
+++ src/components/basicsComponent/SearchLabel.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import { AutoComplete } from "primereact/autocomplete";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
-import { FloatLabel } from "primereact/floatlabel";
 
 export default function SearchLabel({callbackButton, buttonIcon, token}) {
     const [value, setValue] = useState('');
@@ -10,9 +8,8 @@ export default function SearchLabel({callbackButton, buttonIcon, token}) {
     
     return (
         <div className="search">
-                <InputText id="element" value={value} onChange={(e) => setValue(e.target.value)} />
-                <Button icon={icon} rounded outlined severity="success" aria-label="Search" onClick={() => callbackButton(token, value)} />
-            
+                <InputText id="element" placeholder="Inserisci il nome della canzone" value={value} onChange={(e) => setValue(e.target.value)} />
+                <Button id="searchButton" icon={icon} rounded outlined severity="success" aria-label="Search" onClick={() => callbackButton(token, value)} /> 
         </div>
     )
 }
