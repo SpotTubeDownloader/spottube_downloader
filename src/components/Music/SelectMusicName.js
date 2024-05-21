e2fa9da3d46ef898bf0f5c1ca0073ad65434cf85
santal1210 <sant.nino02@gmail.com>
2024-05-21 19:31:34 +0200

    fix SongScroll

--- src/components/Music/SelectMusicName.js
+++ src/components/Music/SelectMusicName.js
@@ -3,19 +3,27 @@ import { Panel } from 'primereact/panel';
 import SearchLabel from '../basicsComponent/SearchLabel';
 import SongScroller from '../basicsComponent/SongScroller';
 import { getSongsList } from '../../service/MusicService';
+import { useState } from 'react';
 
 
 export default function SelectMusicName({ token }) {
+    const [visible, setVisible] = useState(false);
+    const [songs, setSongs] = useState(null);
+    
     const callbackButton = (token, value) => {
         getSongsList(token, value).then((data) => {
             console.log(data);
+            setVisible(true);
+            setSongs(data);
+            console.log(data)
         }).catch((error) => {
             console.log(error);
         });
     }
     return (
         <Panel header="Scarica attraverso il nome" toggleable collapsed={true}>
-            <SearchLabel token={token} labelText={"Inserisci il nome della canzone"} buttonIcon={"search"} callbackButton={callbackButton} />
+            {!visible && <SearchLabel token={token} labelText={"Inserisci il nome della canzone"} buttonIcon={"search"} callbackButton={callbackButton} />}
+            {visible && songs !== null && <SongScroller songs={songs} />}
         </Panel>
     )
 }
