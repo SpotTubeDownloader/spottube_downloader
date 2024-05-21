f23afe4321bf53e350c64fead7cfd6b3b79ce761
santal1210 <sant.nino02@gmail.com>
2024-05-21 19:31:34 +0200

    fix SongScroll

--- src/components/basicsComponent/SearchLabel.js
+++ src/components/basicsComponent/SearchLabel.js
@@ -4,15 +4,14 @@ import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
 
-export default function SearchLabel({callbackButton, buttonIcon, labelText, token}) {
+export default function SearchLabel({callbackButton, buttonIcon, token}) {
     const [value, setValue] = useState('');
     const icon = "pi pi-" + buttonIcon
     
     return (
-        <div className="card flex justify-content-center">
+        <div className="search">
             <FloatLabel>
                 <InputText id="element" value={value} onChange={(e) => setValue(e.target.value)} />
-                <label htmlFor="element">{labelText}</label>
             </FloatLabel>
             <Button icon={icon} rounded outlined severity="success" aria-label="Search" onClick={() => callbackButton(token, value)} />
         </div>
--- src/components/basicsComponent/SongScroller.js
+++ src/components/basicsComponent/SongScroller.js
@@ -5,21 +5,22 @@ import { Button } from 'primereact/button';
 export default function SongScroller({songs}) {
 
     const songsTemplate = (data) => {
+        console.log(data);
         return (
             <div>
                 <div id="boxHistory">
-                    <img id="imgHistory" src={`${data.song.thumbnail}`}/>
+                    <img id="imgHistory" src={`${data.thumbnail}`}/>
                     <div>
                         <div id="boxtitlelink">
                             <div>
-                                <div id="titleHistory">{data.song.title}</div>
+                                <div id="titleHistory">{data.title}</div>
                                 <br></br>
-                                <div><a href={data.song.link} target="blank">Apri su Youtube</a></div>
+                                <div><a href={data.link} target="blank">Apri su Youtube</a></div>
                                 <br></br>
                             </div>
                         </div>
                         <div id="boxButtonHistory">
-                            <Button id="buttonHistory" icon="pi pi-download" label="Scarica" disabled={data.song.name === 'MILLE'}></Button>
+                            <Button id="buttonHistory" icon="pi pi-download" label="Scarica"></Button>
                         </div>
                     </div>
                 </div>
