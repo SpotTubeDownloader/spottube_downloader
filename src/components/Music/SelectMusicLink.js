94e2431cb3a6d6c815f4c806a99652249cb83510
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2024-05-25 10:38:41 +0200

    spinner added

--- src/components/Music/SelectMusicLink.js
+++ src/components/Music/SelectMusicLink.js
@@ -1,41 +1,36 @@
-// import React from 'react'; 
-// import { Panel } from 'primereact/panel';
-
-// export default function SelectMusicLink() {
-//     return (
-//         <Panel header="Scarica attraverso un link" toggleable collapsed={true}>
-//             <p className="m-0">
-//             <input type="text" id="linkMusic" placeholder="Inserisci il link Spotify/Youtube"/>
-//             </p>
-//         </Panel>
-//     )
-// }
-
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import { Panel } from 'primereact/panel';
 import SearchLabel from '../basicsComponent/SearchLabel';
-import SongScroller from '../basicsComponent/SongScroller';
-import { getSongsList } from '../../service/MusicService';
-import { useState } from 'react';
+import { ProgressSpinner } from 'primereact/progressspinner';
 import { downloadSongByYoutubeLink } from '../../service/MusicService';
 import { useAuth0 } from '@auth0/auth0-react';
-
-
 
 export default function SelectMusicLink({ token }) {
     const {user} = useAuth0();
     const [visible, setVisible] = useState(false);
-    const [songs, setSongs] = useState(null);
+    const [loading, setLoading] = useState(null);
     
-    const callbackButton = (token, value) => {
-            downloadSongByYoutubeLink(token, user.sub ,value).then().catch((error)=>{
-                console.log(error);
-            });
+    const callbackButton = async (token, value) => {
+        setLoading(true);
+        try {
+            await downloadSongByYoutubeLink(token, user.sub, value);
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
     }
     return (
-        <Panel header="Scarica attraverso il link" toggleable collapsed={true}>
-            {!visible && <SearchLabel token={token} buttonIcon={"download"} callbackButton={callbackButton} />}
-        </Panel>
+        <>
+            {loading && (
+                <div className="spinner">
+                    <ProgressSpinner style={{ width: '50px', height: '50px' }} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
+                </div>
+            )}
+            <Panel header="Scarica attraverso il link" toggleable collapsed={true}>
+                {!visible && <SearchLabel token={token} buttonIcon={"download"} callbackButton={callbackButton} />}
+            </Panel>
+        </>
     )
 }
         
