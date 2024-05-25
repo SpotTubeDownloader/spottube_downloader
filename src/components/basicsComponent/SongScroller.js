2ea74c658a4c1eaeee0b29f21a2ad21578689fa6
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2024-05-25 10:38:41 +0200

    spinner added

--- src/components/basicsComponent/SongScroller.js
+++ src/components/basicsComponent/SongScroller.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { DataScroller } from 'primereact/datascroller';
 import { Button } from 'primereact/button';
 import {downloadSongByYoutubeLink, } from '../../service/MusicService';
 import {deleteElementinHistoryBySongId} from '../../service/HistoryService';
 import { useAuth0 } from '@auth0/auth0-react';
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 export default function SongScroller({songs, token, isHistory=false}) {
     const {user} = useAuth0();
+    const [loading, setLoading] = useState(false);
+
     const deleteCallback = (id) =>{
         deleteElementinHistoryBySongId(token, user.sub, id).then((data)=>{
             songs = data;
@@ -16,10 +19,15 @@ export default function SongScroller({songs, token, isHistory=false}) {
     }
 
     const songsTemplate = (data) => {
-        const buttonCallback = () => {
-            downloadSongByYoutubeLink(token, user.sub ,data.link).then().catch((error)=>{
-                console.log(error);
-            });
+        const buttonCallback = async () => {
+            setLoading(true);
+            try {
+                await downloadSongByYoutubeLink(token, user.sub, data.link);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                setLoading(false);
+            }
         }
         return (
             <div id="boxHistory">
@@ -48,9 +56,14 @@ export default function SongScroller({songs, token, isHistory=false}) {
     };
 
     return (
-        <div className="card">
+        <>
+            {loading && (
+                <div className="spinner">
+                    <ProgressSpinner style={{ width: '50px', height: '50px' }} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
+                </div>
+            )}
             <DataScroller value={songs} itemTemplate={songsTemplate} rows={1000} inline scrollHeight="auto" emptyMessage="Nessun elemento trovato"/>
-        </div>
+        </>
     )
 }
         
--- src/service/VideoService.js
+++
@@ -1,9 +0,0 @@
-const videoData = [
-    { name: 'Se domani non torno distruggi tutto', code: 'Flavio e Sofia', image: "https://img.youtube.com/vi/5mBFvgPcgzQ/mqdefault.jpg" },
-    { name: 'Il caso del piccione spia arrestato', code: 'BarbascuraX', image: "https://img.youtube.com/vi/2YuSsGPxA0w/mqdefault.jpg"},
-    { name: 'La caduta della fetta di pane', code: 'Viral Lab', image: "https://img.youtube.com/vi/6zSUdmxdGjY/mqdefault.jpg"},
-];
-
-export const VideoService = {
-    getVideo: () => Promise.resolve(videoData)
-};
