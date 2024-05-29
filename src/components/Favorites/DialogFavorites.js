3fdaeead0de49f85460ae80be9e7266895135fb4
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2024-05-29 10:30:17 +0200

    remove video box

--- src/components/Favorites/DialogFavorites.js
+++ src/components/Favorites/DialogFavorites.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Dialog } from 'primereact/dialog';
 import FavoritesBoxMusic from "./MusicFavorites/FavoritesBoxMusic";
-import FavoritesBoxVideo from "./VideoFavorites/FavoritesBoxVideo";
 
 export default function DialogFavorites({ visible, position, onHide }) {
     
@@ -10,9 +9,6 @@ export default function DialogFavorites({ visible, position, onHide }) {
             <Dialog header="Preferiti" visible={visible} position={position} style={{ width: '70vw' }} onHide={onHide} draggable={false} resizable={false} maximizable>
                 <p className="m-0">
                     <FavoritesBoxMusic />
-                    <br></br>
-                    <br></br>
-                    <FavoritesBoxVideo />
                 </p>
             </Dialog>
         </div>
