65eea4b7d66a209d8e9e9b741be5eef953fb196a
santal1210 <sant.nino02@gmail.com>
2024-06-04 00:37:10 +0200

    fix import

--- src/page/Login.js
+++ src/page/Login.js
@@ -3,7 +3,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { saveAs } from 'file-saver';
 
 
 
