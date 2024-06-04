75f4bc745b80d26e81a3130e3ca117ae67e24174
santal1210 <sant.nino02@gmail.com>
2024-06-04 23:47:50 +0200

    add rating

---
+++ src/service/RatingService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api_url = process.env.REACT_APP_API_URL;
+
+export async function getRatingByUserSub(token, userSub){
+    const response = await  axios.get(`${api_url}/user/getRatingByUserSub/${userSub}`,{
+        headers:{
+            authorization: `Bearer ${token}`,
+        }
+    });
+    return response.data;
+}
+
+export async function updateRatingByUserSub(token, userSub, rating){
+    const response = await axios.post(`${api_url}/user/updateRatingByUserSub`,{
+        userSub: userSub,
+        rating: rating
+    },{
+        headers:{
+            authorization: `Bearer ${token}`,
+        }
+    });
+    return response.data;
+}
