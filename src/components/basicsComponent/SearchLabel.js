e383c927ff7110e40076be17bd6a89200f00864f
santal1210 <sant.nino02@gmail.com>
2024-07-01 15:05:49 +0200

    press enter

--- src/components/basicsComponent/SearchLabel.js
+++ src/components/basicsComponent/SearchLabel.js
@@ -7,10 +7,18 @@ import '../../css/scroller.css';
 export default function SearchLabel({callbackButton, buttonIcon, token}) {
     const [value, setValue] = useState('');
     const icon = "pi pi-" + buttonIcon
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            callbackButton(token, value);
+        }
+    };
+
+
     
     return (
         <div className="search">
-                <InputText id="element" value={value} onChange={(e) => setValue(e.target.value)} />
+                <InputText id="element" value={value} onChange={(e) => setValue(e.target.value)} onKeyPress={handleKeyPress} />
                 <Button id="searchButton" icon={icon} rounded outlined severity="success" aria-label="Search" onClick={() => callbackButton(token, value)} /> 
         </div>
     )
